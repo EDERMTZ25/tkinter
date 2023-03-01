2ec9500ad60e0cbac278948e88e896217b5d2b60
EDERMTZ25 <123896637+EDERMTZ25@users.noreply.github.com>
2023-03-01 08:41:15 -0600

    Update pract11.py
    Se agrego un fondo de color a cada seccion de la pagina, ademas se agregaron losn botones y se añadio una ventana de dialogo para un boton

--- pract11.py
+++ pract11.py
@@ -1,33 +1,37 @@
-from tkinter import TK, Button, Frame
+from tkinter import Tk,Button,Frame,messagebox
 
-#1
-ventana=TK()
-ventana.title= ("EJEMPLO DE 3 FRAMES")
+def mostrarMensaje():
+    messagebox.showinfo("aviso:","presionaste el boton azul")
+
+#1. instanciamos el objeto ventana
+ventana= Tk()
+ventana.title("ejemplo de 3 frames")
 ventana.geometry("600x400")
 
+#2. agregamos los frames 
+seccion1= Frame(ventana,bg="blue")
+seccion1.pack(expand=True,fill='both')
 
-#2
-seccion1=Frame(ventana,bg="red")
-seccion1.pack(expand=True, fill='both')
+seccion2= Frame(ventana,bg="red")
+seccion2.pack(expand=True,fill='both')
 
-seccion2=Frame(ventana,bg="green")
-seccion2.pack(expand=True, fill='both')
-seccion3=Frame(ventana,bg="yellow")
-seccion3.pack(expand=True, fill='both')
+seccion3= Frame(ventana,bg="black")
+seccion3.pack(expand=True,fill='both')
 
-#3 Botones
-botonAzul=Button(seccion1, text="Boton Azul", fg="blue")
-botonAzul.place(x="60", y="60")
+#3. agregamos los botones
+btonAzul= Button(seccion1,text="boton Azul",fg="blue",bg="black",command=mostrarMensaje)
+btonAzul.place(x="60",y="60") 
 
-botonnNegro=Button(seccion2, text="Boton Negro", fg="white")
-botonnNegro.grid(row=0, column=0)
+btonAmarillo= Button(seccion2,text="boton Amarillo",fg="yellow",bg="blue")
+btonAmarillo.grid(row= 1, column= 1)
 
-botonVerde=Button(seccion1, text="BotonAzul", fg="blue")
-botonVerde.place(x="60", y="60")
+btonNegro= Button(seccion2,text="boton negro",fg="white",bg="black")
+btonNegro.grid(row= 0, column= 0)
 
-
-
+btonVerde= Button(seccion3,text="boton verde",fg="green",bg="#800080")
+btonVerde.configure(height=2,width=10)
+btonVerde.pack()
 
 
-#4
+#4. Llamamos al main
 ventana.mainloop()
